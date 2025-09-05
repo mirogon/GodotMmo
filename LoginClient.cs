aa85f1a3cebd96a8db93d919cd943790a783384c
Marcel Römer <marcelroemersoftware@gmail.com>
2025-09-05 15:00:15 +0200

    Register/Login stuff

--- LoginClient.cs
+++ LoginClient.cs
@@ -10,7 +10,8 @@ using System.Threading.Tasks;
 
 public class LoginClient
 {
-    public static Action<bool> LoginUpdate;
+    public static Action<bool, string> RegisterUpdate;
+    public static Action<bool, string> LoginUpdate;
 
     static System.Net.Http.HttpClient _httpClient = new();
     public static async Task SignUp(string email, string name, string password)
@@ -20,11 +21,18 @@ public class LoginClient
         var content = new StringContent(json, Encoding.UTF8, "application/json");
         HttpResponseMessage response = await _httpClient.PostAsync("https://localhost:7285/signup", content);
 
-        if (response.IsSuccessStatusCode)
-        {
-        }
-        GD.Print("Sign up succeeded: " + response.IsSuccessStatusCode);
+        var responseContentStr = GetResponseContentStringFromResponse(response);
+        var reqResponse = ResponseContentStringToRequestResponse(responseContentStr);
 
+        if (reqResponse.Success)
+        {
+            RegisterUpdate?.Invoke(true, "");
+        }
+        else
+        {
+            RegisterUpdate?.Invoke(false, reqResponse.Message);
+        }
+        GD.Print("Sign up succeeded: " + reqResponse.Success);
         return;
     }
     public static async Task Login(string email, string password)
@@ -37,35 +45,42 @@ public class LoginClient
             var content = new StringContent(json, Encoding.UTF8, "application/json");
             HttpResponseMessage response = await _httpClient.PostAsync("https://localhost:7285/login", content);
 
-            var stream = response.Content.ReadAsStream();
-            var len = stream.Length;
-            byte[] responseContentBytes = new byte[len];
-            stream.Read(responseContentBytes);
-            string responseContentStr = Encoding.UTF8.GetString(responseContentBytes);
-
-
-            JsonSerializerOptions opt = new();
-            opt.PropertyNameCaseInsensitive = true;
-            var reqResponse = JsonSerializer.Deserialize<RequestResponse>(responseContentStr, opt);
+            var responseContentStr = GetResponseContentStringFromResponse(response);
+            var reqResponse = ResponseContentStringToRequestResponse(responseContentStr);
 
             if (reqResponse.Success)
             {
                 GD.Print("Login succeeded: ");
-                LoginUpdate?.Invoke(true);
+                LoginUpdate?.Invoke(true, "");
             }
             else
             {
                 GD.Print("Login Failed: " + reqResponse.Message);
-                LoginUpdate?.Invoke(false);
+                LoginUpdate?.Invoke(false, reqResponse.Message);
             }
         }
         catch (Exception ex)
         {
             GD.Print("Login Failed: " + ex.Message);
-            LoginUpdate?.Invoke(false);
+            LoginUpdate?.Invoke(false, ex.Message);
         }
 
         return;
     }
 
+    public static string GetResponseContentStringFromResponse(HttpResponseMessage responseMessage)
+    {
+        var stream = responseMessage.Content.ReadAsStream();
+        var len = stream.Length;
+        byte[] responseContentBytes = new byte[len];
+        stream.Read(responseContentBytes);
+        string responseContentStr = Encoding.UTF8.GetString(responseContentBytes);
+        return responseContentStr;
+    }
+    public static RequestResponse ResponseContentStringToRequestResponse(string contentStr)
+    {
+        JsonSerializerOptions opt = new();
+        opt.PropertyNameCaseInsensitive = true;
+        return JsonSerializer.Deserialize<RequestResponse>(contentStr, opt);
+    }
 }
